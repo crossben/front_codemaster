ba48153cd237c8a6000e1f35a01662a96281f096
Ben <gueco167@gmail.com>
2024-10-10 15:27:07 +0000

    added dark mode support

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+// import DarkMode from "./components/mode";
 import { ClerkProvider } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Import your publishable key
 const PUBLISHABLE_KEY = "pk_test_dmFzdC1pbXBhbGEtODkuY2xlcmsuYWNjb3VudHMuZGV2JA"
@@ -11,10 +13,30 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
+const App: React.FC = () => {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+      <RouterProvider router={router} />
     </ClerkProvider>
   </StrictMode>,
 )
